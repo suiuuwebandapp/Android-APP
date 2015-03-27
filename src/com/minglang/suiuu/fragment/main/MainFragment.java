b87194268aa99bc079c4d3873e3c41c722d1e918
LiuZhenYu <liuzhenyu26@live.cn>
2015-03-27 09:59:42 +0800

    添加新Lib

--- src/com/minglang/suiuu/fragment/main/MainFragment.java
+++ src/com/minglang/suiuu/fragment/main/MainFragment.java
@@ -1,12 +1,14 @@
 package com.minglang.suiuu.fragment.main;
 
 import com.minglang.suiuu.R;
+
 import android.annotation.SuppressLint;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 
@@ -15,23 +17,36 @@ import android.widget.ListView;
  */
 public class MainFragment extends Fragment {
 
-    private ListView mianPageListView;
+    private ListView mainPageListView;
 
-	@SuppressLint("InflateParams")
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+    @SuppressLint("InflateParams")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-		View rootView = inflater.inflate(R.layout.fragment_main, null);
+        View rootView = inflater.inflate(R.layout.fragment_main, null);
 
         initView(rootView);
 
-		return rootView;
-	}
+        ViewAction();
 
-    /**初始化方法*/
-    private void initView(View rootView){
-        mianPageListView = (ListView) rootView.findViewById(R.id.mainPageListView);
+        return rootView;
+    }
+
+    private void ViewAction() {
+        mainPageListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
+    /**
+     * 初始化方法
+     */
+    private void initView(View rootView) {
+        mainPageListView = (ListView) rootView.findViewById(R.id.mainPageListView);
     }
 
 }
